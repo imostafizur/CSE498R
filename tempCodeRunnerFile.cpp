bb352b2f2984a350816e497032406b4b1f89dfbf
Mostafizur Rahman <59665707+imostafizur@users.noreply.github.com>
2022-09-30 08:25:37 +0600

    Jason file

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,12 @@
+// Add two numbers in c++
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, sum;
+    cout << "Enter two numbers to add : ";
+    cin >> a >> b;
+    sum = a + b;
+    cout << "Sum of the numbers is : " << sum;
+    return 0;
+}
